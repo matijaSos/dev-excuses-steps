6e5d98ee4dfcb0976274fb996f26be33ad18c2c9
Matija Sosic <matija.sosic@gmail.com>
2022-08-24 11:24:32 +0000

    step 1: fetch and show an excuse from external API

---
+++ MyNewApp/ext/queries.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const getExcuse = async () => {
+  return axios
+    .get('https://api.devexcus.es/')
+    .then(res => {
+      return res.data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
